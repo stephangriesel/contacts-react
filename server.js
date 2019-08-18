b5a91cd196973fd243e4303bf5072c5576e6092e
Stephan <sgriesel@gmail.com>
2019-08-18 06:07:12 +0200

    set up routes for backend

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to Contacts App Api' }))
 
+// Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
