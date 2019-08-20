cdd7f8031b41c180c1023ab74cc6dd2d9b398332
Stephan <sgriesel@gmail.com>
2019-08-20 05:53:56 +0200

    update route with new model

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 
 router.post('/', (req, res) => {
-    res.send('Register user')
+    res.send(req.body)
 });
 
 module.exports = router;
