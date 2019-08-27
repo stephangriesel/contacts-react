0de81d17aab90a59cf429e6aa02f7f74fc61bdb0
Stephan <sgriesel@gmail.com>
2019-08-27 06:22:11 +0200

    update protected route - continue

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,15 @@ const User = require('../models/User');
 // @desc    Get logged in user
 // @access  Private
 
-router.get('/', auth,
-    (req, res) => {
-        res.send('Get logged in user')
-    });
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 // @route   POST api/auth
 // @desc    Auth user & get token
