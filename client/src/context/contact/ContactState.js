70795cc606984f8fb1c24bdf12fb95e170860cf7
Stephan <sgriesel@gmail.com>
2019-09-17 06:44:48 +0200

    add filter

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -44,7 +44,8 @@ const ContactState = props => {
                 type: 'professional'
             }
         ],
-        current: null
+        current: null,
+        filtered: null
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -76,18 +77,26 @@ const ContactState = props => {
     }
 
     // Filter contacts
-
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text });
+    }
     // Clear filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER });
+    }
 
     return (
         <ContactContext.Provider value={{
             contacts: state.contacts,
             current: state.current,
+            filtered: state.filtered,
             addContact,
             deleteContact,
             setCurrent,
             clearCurrent,
-            updateContact
+            updateContact,
+            filterContacts,
+            clearFilter
         }}>
             {props.children}
         </ContactContext.Provider>
