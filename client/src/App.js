72edbb26a20c34962a209d3636bdcab32cbc1800
Stephan <sgriesel@gmail.com>
2019-09-18 06:25:09 +0200

    add register & login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/NavBar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 
@@ -19,6 +22,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
