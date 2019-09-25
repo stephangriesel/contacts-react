f53a258c0721e0a7e527041f7b04aaaec73d44ea
Stephan <sgriesel@gmail.com>
2019-09-25 05:27:28 +0200

    add  login logic

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -66,7 +66,28 @@ const AuthState = props => {
     }
 
     // Login User
-    const loginUser = () => console.log('login user');
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.post('/api/auth', formData, config);
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            });
+
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err.response.data.msg
+            });
+        }
+    }
 
     // Logout User
     const logoutUser = () => console.log('logout user');
@@ -84,7 +105,7 @@ const AuthState = props => {
             error: state.error,
             register,
             loadUser,
-            loginUser,
+            login,
             logoutUser,
             clearErrors
         }}>
